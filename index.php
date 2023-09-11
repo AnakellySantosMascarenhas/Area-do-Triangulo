ffae249e04c79083350ab09e2547698baa14f4d8
Kelly Santos <105869919+AnakellySantosMascarenhas@users.noreply.github.com>
2023-09-11 17:46:35 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Area do Triangulo</title>
+</head>
+<body>
+<form action="resultado.php" method="post">  Calcule a área do triângulo : 
+       <br> 
+       Digite a altura: <input type="Textarea" name="altura"><br> 
+       Digite a base do triângulo: <input type="Textarea" name="base"><br>
+       <input type="submit"> 
+</form> 
+</body>
+</html>
